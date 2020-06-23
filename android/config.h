075ac169c42f18a382addaef0d53dc004145c528
hmtheboy154 <buingoc67@gmail.com>
2020-06-23 16:35:36 +0700

    TEMP HACK : not define HAVE_SYS_SHM_H
    API25 (nougat) doesn't have sys/shm.h yet.

--- android/config.h
+++ android/config.h
@@ -107,7 +107,7 @@
 /* #undef HAVE_SYS_ENDIAN_H */
 
 /* Define to 1 if you have the <sys/shm.h> header file. */
-#define HAVE_SYS_SHM_H 1
+/* #undef HAVE_SYS_SHM_H */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
